0f972d8f1986294fdf67174dd3089fbf49e48a0c
JantzAnto <anton.jantzen@live.de>
2019-06-20 19:59:00 +0200

    Initial Commit

--- app/src/main/java/com/example/mobileapp_praktikum/MainActivity.kt
+++ app/src/main/java/com/example/mobileapp_praktikum/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.mobileapp_praktikum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+//Commit function test
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
