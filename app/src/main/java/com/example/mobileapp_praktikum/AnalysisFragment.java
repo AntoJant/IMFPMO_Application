b17c6f57b7016c36b0879b1a91492bde612d759f
JantzAnto <anton.jantzen@live.de>
2019-07-01 07:56:47 +0200

    - Added code support for spinner listeners - Added spinner initialization to select fragments

--- app/src/main/java/com/example/mobileapp_praktikum/AnalysisFragment.java
+++ app/src/main/java/com/example/mobileapp_praktikum/AnalysisFragment.java
@@ -10,7 +10,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -35,6 +38,24 @@ public class AnalysisFragment extends Fragment {
         BottomNavigationView bottomNav = view.findViewById(R.id.bottom_navigation);
         bottomNav.setSelectedItemId(R.id.nav_analysis);
         ((MainActivity)getActivity()).FragmentListener(bottomNav);
+
+        Spinner spinner = (Spinner) view.findViewById(R.id.analysis_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getActivity(), "Selected " + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getActivity(), "Selected " , Toast.LENGTH_LONG).show();
+
+            }
+        });
 
         //must happen only once. maybe check if service already running
         Context context = ((MainActivity)getActivity());
--- app/src/main/java/com/example/mobileapp_praktikum/WaysFragment.java
+++ app/src/main/java/com/example/mobileapp_praktikum/WaysFragment.java
@@ -8,7 +8,10 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -31,7 +34,25 @@ public class WaysFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_ways, container, false);
         BottomNavigationView bottomNav = view.findViewById(R.id.bottom_navigation);
         bottomNav.setSelectedItemId(R.id.nav_ways);
+
         Spinner spinner = (Spinner) view.findViewById(R.id.ways_spinner);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getActivity(), android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String item = parent.getItemAtPosition(position).toString();
+                Toast.makeText(getActivity(), "Selected " + item, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getActivity(), "Selected " , Toast.LENGTH_LONG).show();
+
+            }
+        });
+
         ((MainActivity)getActivity()).FragmentListener(bottomNav);
 
         return view;
